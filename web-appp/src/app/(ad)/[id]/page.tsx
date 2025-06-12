a875722de9471319705b0dec3facb8ed42323623
bte3108 <info@biotrendenergy.com>
2025-06-12 16:07:52 +0530

    fix: update layout metadata and improve global styles for better branding

--- web-appp/src/app/(ad)/[id]/page.tsx
+++ web-appp/src/app/(ad)/[id]/page.tsx
@@ -31,41 +31,48 @@ export default async function Home(props: PageProps) {
     type: "ad",
   });
   return (
-    <main className="w-full h-auto px-5 lg:px-[150px] py-2.5">
-      <div className="py-2.5">
-        <Billboard />
-      </div>
-      <div className="flex flex-col lg:flex-row lg:gap-3">
-        <div className="w-full lg:w-8/12  flex gap-1.5 flex-col">
-          <VideoPlayer
-            poster={data.poster || undefined}
-            url={data.url || undefined}
-          />
-          <div className="border-b border-b-black py-2.5 px-2">{data.name}</div>
-          <div className="my-2.5">
-            <Billboard />
-          </div>
+    <>
+      <head>
+        <title>{data.name}</title>
+      </head>
+      <main className="w-full h-auto px-5 lg:px-[150px] py-2.5">
+        <div className="py-2.5">
+          <Billboard />
         </div>
+        <div className="flex flex-col lg:flex-row lg:gap-3">
+          <div className="w-full lg:w-8/12  flex gap-1.5 flex-col">
+            <VideoPlayer
+              poster={data.poster || undefined}
+              url={data.url || undefined}
+            />
+            <div className="border-b border-b-black py-2.5 px-2">
+              {data.name}
+            </div>
+            <div className="my-2.5">
+              <Billboard />
+            </div>
+          </div>
 
-        <div className="mt-2.5 lg:w-4/12">
-          <h1>More videos</h1>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-            {modifiedDb.map((v, i) =>
-              "type" in v && v.type === "ad" ? (
-                <VideoAd key={`ad-${i}`} />
-              ) : // Only pass v to VideoPost if it has the expected post properties
-              "id" in v &&
-                "name" in v &&
-                "poster" in v &&
-                "url" in v &&
-                "created_at" in v &&
-                "view" in v ? (
-                <VideoPost data={v} notShow={data.id} key={v.id || i} />
-              ) : null
-            )}
+          <div className="mt-2.5 lg:w-4/12">
+            <h1>More videos</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+              {modifiedDb.map((v, i) =>
+                "type" in v && v.type === "ad" ? (
+                  <VideoAd key={`ad-${i}`} />
+                ) : // Only pass v to VideoPost if it has the expected post properties
+                "id" in v &&
+                  "name" in v &&
+                  "poster" in v &&
+                  "url" in v &&
+                  "created_at" in v &&
+                  "view" in v ? (
+                  <VideoPost data={v} notShow={data.id} key={v.id || i} />
+                ) : null
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
